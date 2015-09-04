761f64dfd9f3590a84dbd31e9fa1f8edeeae9f2c
Matt <mgfredrickson@nevada.unr.edu>
2015-09-04 15:19:58 -0700

    added system arg interpretation, improved help function

--- gist-cli.py
+++ gist-cli.py
@@ -2,27 +2,31 @@ import sys
 
 # Links input text to relevant function/operation
 def interpret_cmd(arg):
+	# build a dictionary of commands to functions
 	switch = {
 		'a': toString('hello'),
 		'b': toString( 12 ),
 	}
-	return switch.get( arg, "Command \'{0}\' not recognized...\nAvailable commands: {1}".format(arg,printHelp(switch.items())) )
 	
-# Gist function definitions
+	# execute function call or return help message
+	return switch.get( arg, printHelp(arg,switch.items()) )
+
+# Print command error & help message (available commands)
+def printHelp(c, arr):
+	help = ""			# start blank to avoid prepended commas
+	for key,val in arr:
+		if help != "":	# add the comma separation! (only when items are already listed though)
+			help += ", "
+		else:			# string is empty so start building
+			help = "Command \'{0}\' not recognized...\nAvailable commands: ".format(c)
+		help += key		# finally, add the command string itself
+	return help
+
+# Gist-CLI function definitions
 def toString(arg):
 	return str(arg)
 
-def printHelp(arr):
-	help = ""
-	for key,val in arr:
-		if help != "":
-			help += ", " # comma separation!
-		help += key
-	return help
-
-# Program 
-#cmds = process.argv.split()
-#if len(cmds) > 0:
-#	print interpret_cmd( cmds[0] )
-for s in sys.argv:
-	print s
+# System-level entry point
+cmds = sys.argv[1:]
+if len(cmds) > 0:
+	print interpret_cmd( cmds[0] )
